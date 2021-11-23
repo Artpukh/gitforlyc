d2ef11c14c323c66f55f384234ed63679e41dbf8
Артем <apuhtinar@yandex.ru>
2021-11-23 17:59:53 +0300

    yellow_circle

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print('My first git program')
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class Yellow_Circle(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('UI.ui', self)
+        self.paint = False
+        self.push.clicked.connect(self.run)
+
+    def run(self):
+        self.paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        x, y = randint(0, 600), randint(0, 400)
+        diam = randint(0, 300)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(x, y, diam, diam)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Yellow_Circle()
+    ex.show()
+    sys.exit(app.exec())
+
